613cc7c6b0d867ce6388a9b6ced6d8666bf71a3b
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-10-05 17:44:47 -0600

    Carousel de Partners Terminado

---
+++ js/partnersCarousel.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  $(".partners-carousel").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          autoplaySpeed: 1500,
+        },
+      },
+    ],
+  });
+});
